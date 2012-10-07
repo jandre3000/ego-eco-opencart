9e581a4067fd480d83c76f7f5d0d0735e21ddd2d
Jan Drewniak <jan.drewniak@gmail.com>
2012-10-07 13:19:23 +0200

    initial commit

---
+++ admin/config.php
@@ -0,0 +1,32 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://ego_eco.dev/admin/');
+define('HTTP_CATALOG', 'http://ego_eco.dev/');
+define('HTTP_IMAGE', 'http://ego_eco.dev/image/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://ego_eco.dev/admin/');
+define('HTTPS_CATALOG', 'http://ego_eco.dev/');
+define('HTTPS_IMAGE', 'http://ego_eco.dev/image/');
+
+// DIR
+define('DIR_APPLICATION', '/root/opencart/admin/');
+define('DIR_SYSTEM', '/root/opencart/system/');
+define('DIR_DATABASE', '/root/opencart/system/database/');
+define('DIR_LANGUAGE', '/root/opencart/admin/language/');
+define('DIR_TEMPLATE', '/root/opencart/admin/view/template/');
+define('DIR_CONFIG', '/root/opencart/system/config/');
+define('DIR_IMAGE', '/root/opencart/image/');
+define('DIR_CACHE', '/root/opencart/system/cache/');
+define('DIR_DOWNLOAD', '/root/opencart/download/');
+define('DIR_LOGS', '/root/opencart/system/logs/');
+define('DIR_CATALOG', '/root/opencart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'rootytooty');
+define('DB_DATABASE', 'ego_eco');
+define('DB_PREFIX', '');
+?>
---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://ego_eco.dev/');
+define('HTTP_IMAGE', 'http://ego_eco.dev/image/');
+define('HTTP_ADMIN', 'http://ego_eco.dev/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://ego_eco.dev/');
+define('HTTPS_IMAGE', 'http://ego_eco.dev/image/');
+
+// DIR
+define('DIR_APPLICATION', '/root/opencart/catalog/');
+define('DIR_SYSTEM', '/root/opencart/system/');
+define('DIR_DATABASE', '/root/opencart/system/database/');
+define('DIR_LANGUAGE', '/root/opencart/catalog/language/');
+define('DIR_TEMPLATE', '/root/opencart/catalog/view/theme/');
+define('DIR_CONFIG', '/root/opencart/system/config/');
+define('DIR_IMAGE', '/root/opencart/image/');
+define('DIR_CACHE', '/root/opencart/system/cache/');
+define('DIR_DOWNLOAD', '/root/opencart/download/');
+define('DIR_LOGS', '/root/opencart/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'rootytooty');
+define('DB_DATABASE', 'ego_eco');
+define('DB_PREFIX', '');
+?>
